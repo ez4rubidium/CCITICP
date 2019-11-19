17f633b99fda027e8945bf8d821284f1f204f98d
hi009900 <57838531+hi009900@users.noreply.github.com>
2019-11-19 20:12:46 +0800

    Update modItem.py

--- modItem.py
+++ modItem.py
@@ -2,4 +2,14 @@
 
 def main():
     print(__file__.split('/')[-1])
-    
+    def main():
+    number=int(input("What record do you want to update?"))
+    step1(number)
+def step1(number):
+    a=open("inventory.txt","r")
+    b=a.readlines()
+    if b==number:
+        data=input("What item do you want to change?")
+        if data=="Record number":
+            
+        
