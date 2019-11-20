7d81b4d6b3efa5cba8d630e6568b391499fb2635
hi009900 <57838531+hi009900@users.noreply.github.com>
2019-11-20 23:18:26 +0800

    Update modItem.py

--- modItem.py
+++ modItem.py
@@ -2,14 +2,38 @@
 
 def main():
     print(__file__.split('/')[-1])
-def main1():
-    number=int(input("What record do you want to update?"))
-    step1(number)
-def step1(number):
-    a=open("inventory.txt","r")
-    b=a.readlines()
-    if b==number:
-        data=input("What item do you want to change?")
-        if data=="Record number":
-            
+g=int(input("What record number do you want to update?"))
+g=g-1001
+a=open("check.txt","r")
+n=g*10
+for h in range(n):
+        e=a.readline()
+i=input("What items in this record you want to change?")
+bb=input("What new information would you set ?")
+if i=="record number":
+    aa=a.readline()
+    aa=bb
+    c=a.read()
+    a.close()
+    b=open("check.txt","w")
+    y=b.write(bb)
+    yy=b.write("\n")
+    oo=b.write(c)
+    b.close()
+elif i=="Item name":
+    for nn in range(1):
+        cc=a.readline()
+    dd=a.readline()
+    dd=bb
+    c=a.read()
+    a.close()
+    b=open("check.txt","w")
+    y=b.write(cc)
+    oo=b.write(dd)
+    yy=b.write("\n")
+    zz=b.write(c)
+    b.close()
+else:
+    print(aaa)
+
         
