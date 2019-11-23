8d0b65c047fe056b366ffd533aa9aeb51b9973c8
hi009900 <57838531+hi009900@users.noreply.github.com>
2019-11-23 20:32:36 +0800

    Update modItem.py

--- modItem.py
+++ modItem.py
@@ -2,39 +2,21 @@
 
 def main():
     print(__file__.split('/')[-1])
-g=int(input("What record number do you want to update?"))
-g=g-1001
-a=open("check.txt","r")
-n=g*10
-for h in range(n):
-        e=a.readline()
-i=input("What items in this record you want to change?")
-bb=input("What new information would you set ?")
-if i=="record number":
-    aa=a.readline()
-    aa=bb
-    c=a.read()
+def readandwrite():
+    a=open("check.txt","r")
+    b=a.readlines()
     a.close()
-    b=open("check.txt","w")
-    y=b.write(bb)
-    yy=b.write("\n")
-    oo=b.write(c)
-    b.close()
-elif i=="Item name":
-    for nn in range(1):
-        cc=a.readline()
-    dd=a.readline()
-    dd=bb
-    c=a.read()
-    a.close()
-    b=open("check.txt","w")
-    y=b.write(cc)
-    oo=b.write(dd)
-    yy=b.write("\n")
-    zz=b.write(c)
-    b.close()
-else:
-    print(aaa)
-   #have not finish
+    c=int(input("Which number do you want to modify?"))
+    d=c-1
+    b[d]=input("Whant content do you want to update?")
+    b[d]=b[d]+"\n"
+    print(b)
+    index=0
+    e=open("check.txt","w")
+    while index<29:
+        e.write(b[index])
+        index=index+1
+    e.close()
+readandwrite()
 
         
